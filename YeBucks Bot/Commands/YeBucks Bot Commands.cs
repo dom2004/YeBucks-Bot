36e5f50e29dce5e45c9afde626415c1797014430
DOM MAGIERA <D.MAGIERA-2022@hull.ac.uk>
2024-02-07 01:27:41 +0000

    Add project files.

---
+++ YeBucks Bot/Commands/YeBucks Bot Commands.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using System.Threading.Tasks;
+
+namespace YeBucks_Bot.Commands
+{
+    public class YeBucks_Bot_Commands : BaseCommandModule
+    {
+        [Command("ban")]
+        public async Task Ban
+        {
+            
+        }
+    }
+}
---
+++ YeBucks Bot/Program.cs
@@ -0,0 +1,54 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System.Threading.Tasks;
+using YeBucks_Bot.Commands;
+using YeBucks_Bot.Config;
+
+namespace YeBucks_Bot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var JsonReader = new JSONreader();
+            await JsonReader.ReadJSON();
+
+            var DiscordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = JsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+
+            Client = new DiscordClient(DiscordConfig);
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { JsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false,
+                
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<YeBucks_Bot_Commands>();
+
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+        private static Task ClientReady(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
+
